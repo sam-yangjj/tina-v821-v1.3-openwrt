3367346f0e1223c270816c3b1670a0663bfd0148
sam-yangjj <yq4139@gmail.com>
2026-04-06 10:44:26 +0800

    add tar cmd

--- package/allwinner/system/busybox-init-base-files/files/etc/init.d/movo.sh
+++ package/allwinner/system/busybox-init-base-files/files/etc/init.d/movo.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-touch /tmp/test
+df -h
+sleep 2
+ln -s /mnt/extsd /userdata
+/etc/init.d/dropbear start
+sleep 2
+/usr/sbin/dropbear -p 22
+/etc/init.d/sync.sh &
+# /userdata/sample_smartIPC_demo -path sample_smartIPC_demo.conf &
+wifi -c Xiaomi_E93C Sam123456789
